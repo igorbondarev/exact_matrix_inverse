f5074c659ae8313177a0add39b9dd46222f47bf6
Igor Bondarev <igor.bondarev@hotmail.com>
2017-07-05 21:18:25 +0300

    Add vector Z calc

--- inverse.py
+++ inverse.py
@@ -38,20 +38,59 @@ def mod(val, m):
 		return val % m
 
 def matrix_by_mod(m, modulo, n):
+	ret = [[0 for x in range(dimension)] for y in range(dimension)]
 	for i in range(n):
 		for j in range(n):
-			m[i][j] = mod(m[i][j], modulo)
+			ret[i][j] = mod(m[i][j], modulo)
+	return ret
+
+def vec_by_mod(vec, modulo):
+	ret = [0 for x in range(len(vec))]
+	for i in range(len(vec)):
+		ret[i] = mod(vec[i], modulo)
+	return Matrix(ret)
 		
 print "Matrix A:"
 print mat
 print "Vector b:"
 print b
 print "Ap:"
-matrix_by_mod(mat, p, dimension)
+mod_matrix = matrix_by_mod(mat, p, dimension)
 print mat
 
-A = Matrix(mat)
+A = Matrix(mod_matrix)
 A_inv = A.inv_mod(5)
 new_mat = [[A_inv[y, x] for x in range(dimension)] for y in range(dimension)]
 print "Cp:"
 print new_mat
+
+print "Starting math"
+print "*************"
+print ""
+
+bk =  [0 for x in range(m)]
+bkp = [0 for x in range(m)]
+xkp = [0 for x in range(m)]
+Matrix_Cp = Matrix(new_mat)
+Matrix_A = Matrix(mat)
+bk[0] = Matrix(b)
+Z = Matrix([[0], [0]])
+
+
+for k in range(m - 1):
+	bkp[k] = vec_by_mod(bk[k], p)
+	xkp[k] = vec_by_mod(Matrix_Cp * bkp[k], p)
+	bk[k + 1] = (bk[k] - Matrix_A * xkp[k]) / p
+
+	print k
+	print bkp[k]
+	print xkp[k]
+	print bk[k + 1]
+
+print "Calc Z"
+print "******"
+
+for k in range(5):
+	Z = Z + (p ** k) * xkp[k]
+
+print Z
